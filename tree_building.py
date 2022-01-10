c29eecfb1419fc3e142d6ea2f2d2ada7682e67d4
rick-peterson <h_sbaity@hotmail.com>
2022-01-10 12:04:10 +0200

    final version

--- tree_building.py
+++ tree_building.py
@@ -9,31 +9,30 @@ class Node:
         self.node_id = node_id
         self.children = []
 
-    def __str__(self):
+    def __repr__(self):
         return f"{self.node_id}: {self.children}"
 
 
 def BuildTree(records):
     root = None
     trees = []
+    records.sort(key=lambda x: x.record_id)
     sorted_record_id = sorted([i.record_id for i in records])
     for i in range(len(records)):
         if i in sorted_record_id and sorted_record_id[-1] == len(records) - 1:
             for j, k in [(i.record_id, i.parent_id) for i in records]:
                 if j < k:
                     raise ValueError ("Node record_id should be smaller than it's parent_id.")
-                elif j == k and (j!=0 or k!=0):
+                elif j == k and (j!= 0 or k!=0):
                     raise ValueError ("Only root should have equal record and parent id.")
         else:
             raise ValueError("Record id is invalid or out of order.")
-    
-    for i in sorted_record_id:
-        trees.append(Node(i))
-        for j in records:
-            if i == j.parent_id:
-                node = Node(i)
-                node.children.append(Node(j.record_id))
-                print(node)
+
+    for i in records:
+        node = Node(i.record_id)
+        trees.append(node)
+        if i.record_id != 0:            
+            trees[i.parent_id].children.append(node)
     print(trees)
     if len(trees) > 0:
         root = trees[0]
@@ -51,6 +50,3 @@ records = [
         ]
 root = BuildTree(records)
 print(root)
-
-
-
